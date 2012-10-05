259415df82e9adf1279da35ca4efb5bc6a2e84be
nikolay.grozev@gmail.com <nikolay.grozev@gmail.com>
2012-10-05 17:40:25 +1000

    Maven prototype of the project

---
+++ src/main/java/org/cloudbus/App.java
@@ -0,0 +1,13 @@
+package org.cloudbus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
