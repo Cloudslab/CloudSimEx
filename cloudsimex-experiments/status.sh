ccf0b0cc19cfe5b36975c022177efbc72f6a19f8
Ubuntu <ubuntu@ip-10-240-48-241.ap-southeast-2.compute.internal>
2013-10-02 08:20:13 +0000

    A bash file to keep track fo the status of the experiment.

---
+++ cloudsimex-experiments/status.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "=================== Experiment status ......"
+sLen=15
+eLen=5 
+if [ ! -z "$1" ]
+  then
+    sLen=$1
+fi
+
+if [ ! -z "$2" ]
+  then
+    eLen=$2
+fi
+
+
+
+for dir in `ls multi-cloud/stat/ | grep wld-*`
+do
+	if [ -d multi-cloud/stat/$dir ]
+	then
+	    echo "============================================================================================ "
+	    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> - " $dir " - <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< "
+	    
+	    echo "Files:"
+	    echo "    " `ls -lah multi-cloud/stat/$dir | egrep "\.log$" | cut -b 1,14-128` 
+	    echo
+	    head -n $sLen multi-cloud/stat/$dir/MultiCloudFramework.log
+	    echo "      ....       "
+	    echo
+	    tail -n $eLen multi-cloud/stat/$dir/MultiCloudFramework.log
+	    echo 
+	    echo
+	    echo
+	fi
+done
+
+
+
+
