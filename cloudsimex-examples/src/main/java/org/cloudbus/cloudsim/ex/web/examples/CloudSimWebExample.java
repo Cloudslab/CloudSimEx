25393c64e7ec741076e9ffa6ef3ad3a5fd18bdce
nikolay.grozev@gmail.com <nikolay.grozev@gmail.com>
2014-06-18 18:34:08 +1000

    Fixed web example - session was submitted before VMs are booted.

--- cloudsimex-examples/src/main/java/org/cloudbus/cloudsim/ex/web/examples/CloudSimWebExample.java
+++ cloudsimex-examples/src/main/java/org/cloudbus/cloudsim/ex/web/examples/CloudSimWebExample.java
@@ -73,172 +73,171 @@ public class CloudSimWebExample {
      */
     public static void main(final String[] args) throws SecurityException, IOException {
 
-	// Step 0: Set up the logger
-	Properties props = new Properties();
-	try (InputStream is = Files.newInputStream(Paths.get("../custom_log.properties"))) {
-	    props.load(is);
-	}
-	CustomLog.configLogger(props);
+        // Step 0: Set up the logger
+        Properties props = new Properties();
+        try (InputStream is = Files.newInputStream(Paths.get("../custom_log.properties"))) {
+            props.load(is);
+        }
+        CustomLog.configLogger(props);
+        CustomLog.printLine("Example of Web session modelling.");
 
-	CustomLog.printLine("Example of Web session modelling.");
+        try {
+            // Step1: Initialize the CloudSim package. It should be called
+            // before creating any entities.
+            int numBrokers = 1;         // number of brokers we'll be using
+            boolean trace_flag = false; // mean trace events
 
-	try {
-	    // Step1: Initialize the CloudSim package. It should be called
-	    // before creating any entities.
-	    int numBrokers = 1; // number of brokers we'll be using
-	    boolean trace_flag = false; // mean trace events
+            // Initialize CloudSim
+            CloudSim.init(numBrokers, Calendar.getInstance(), trace_flag);
 
-	    // Initialize CloudSim
-	    CloudSim.init(numBrokers, Calendar.getInstance(), trace_flag);
+            // Step 2: Create Datacenters - the resource providers in CloudSim
+            Datacenter datacenter0 = createDatacenter("WebDataCenter");
 
-	    // Step 2: Create Datacenters - the resource providers in CloudSim
-	    Datacenter datacenter0 = createDatacenter("WebDataCenter");
+            // Step 3: Create Broker
+            WebBroker broker = new WebBroker("Broker", 5, 10000, datacenter0.getId());
 
-	    // Step 3: Create Broker
-	    WebBroker broker = new WebBroker("Broker", 5, 10000, datacenter0.getId());
+            // Step 4: Create virtual machines
+            List<Vm> vmlist = new ArrayList<Vm>();
 
-	    // Step 4: Create virtual machines
-	    List<Vm> vmlist = new ArrayList<Vm>();
+            // VM description
+            int mips = 250;
+            int ioMips = 200;
+            long size = 10000;  // image size (MB)
+            int ram = 512;      // vm memory (MB)
+            long bw = 1000;
+            int pesNumber = 1;  // number of cpus
+            String vmm = "Xen"; // VMM name
 
-	    // VM description
-	    int mips = 250;
-	    int ioMips = 200;
-	    long size = 10000; // image size (MB)
-	    int ram = 512; // vm memory (MB)
-	    long bw = 1000;
-	    int pesNumber = 1; // number of cpus
-	    String vmm = "Xen"; // VMM name
+            // create two VMs
+            HddVm appServerVM = new HddVm("App-Srv", broker.getId(), mips, ioMips, pesNumber, ram, bw, size, vmm,
+                    new HddCloudletSchedulerTimeShared(), new Integer[0]);
 
-	    // create two VMs
-	    HddVm appServerVM = new HddVm("App-Srv", broker.getId(), mips, ioMips, pesNumber,
-		    ram, bw, size, vmm, new HddCloudletSchedulerTimeShared(), new Integer[0]);
+            HddVm dbServerVM = new HddVm("Db-Srv", broker.getId(), mips, ioMips, pesNumber, ram, bw, size, vmm,
+                    new HddCloudletSchedulerTimeShared(), new Integer[0]);
 
-	    HddVm dbServerVM = new HddVm("Db-Srv", broker.getId(), mips, ioMips, pesNumber,
-		    ram, bw, size, vmm, new HddCloudletSchedulerTimeShared(), new Integer[0]);
+            ILoadBalancer balancer = new SimpleWebLoadBalancer(1, "127.0.0.1", Arrays.asList(appServerVM),
+                    new SimpleDBBalancer(dbServerVM));
+            broker.addLoadBalancer(balancer);
 
-	    ILoadBalancer balancer = new SimpleWebLoadBalancer(
-		    1, "127.0.0.1", Arrays.asList(appServerVM), new SimpleDBBalancer(dbServerVM));
-	    broker.addLoadBalancer(balancer);
+            // add the VMs to the vmList
+            vmlist.add(appServerVM);
+            vmlist.add(dbServerVM);
 
-	    // add the VMs to the vmList
-	    vmlist.add(appServerVM);
-	    vmlist.add(dbServerVM);
+            // submit vm list to the broker
+            broker.submitVmList(vmlist);
 
-	    // submit vm list to the broker
-	    broker.submitVmList(vmlist);
+            List<WebSession> sessions = generateRandomSessions(broker, 100);
+            
+            // Submit the sessions with 1 sec delay, to wait for the VMs to boot.
+            // Note if have a delay policy (see IVMBootDelayDistribution) you may
+            // need to delay the submission with more.
+            broker.submitSessionsAtTime(sessions, balancer.getAppId(), 1);
 
-	    List<WebSession> sessions = generateRandomSessions(broker, 100);
-	    broker.submitSessions(sessions, balancer.getAppId());
+            // Sixth step: Starts the simulation
+            CloudSim.startSimulation();
 
-	    // Sixth step: Starts the simulation
-	    CloudSim.startSimulation();
+            // Final step: Print results when simulation is over
+            List<Cloudlet> newList = broker.getCloudletReceivedList();
 
-	    // Final step: Print results when simulation is over
-	    List<Cloudlet> newList = broker.getCloudletReceivedList();
+            CloudSim.stopSimulation();
 
-	    CloudSim.stopSimulation();
+            printCloudletList(newList);
 
-	    printCloudletList(newList);
+            // Print the debt of each user to each datacenter
+            // datacenter0.printDebts();
 
-	    // Print the debt of each user to each datacenter
-	    // datacenter0.printDebts();
-
-	    System.err.println("\nSimulation is finished!");
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    System.err.println("The simulation has been terminated due to an unexpected error");
-	}
+            System.err.println("\nSimulation is finished!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("The simulation has been terminated due to an unexpected error");
+        }
     }
 
-    private static List<WebSession> generateRandomSessions(final DatacenterBrokerEX broker,
-	    final int sessionNum) {
-	// Create Random Generators
-	Random rng = new MersenneTwisterRNG();
-	GaussianGenerator cpuGen = new GaussianGenerator(1000, 20, rng);
-	GaussianGenerator ramGen = new GaussianGenerator(5, 1, rng);
-	GaussianGenerator ioGen = new GaussianGenerator(1000, 20, rng);
-	NumberGenerator<Integer> modifiesDataGen = new ConstantGenerator<>(0);
+    private static List<WebSession> generateRandomSessions(final DatacenterBrokerEX broker, final int sessionNum) {
+        // Create Random Generators
+        Random rng = new MersenneTwisterRNG();
+        GaussianGenerator cpuGen = new GaussianGenerator(1000, 20, rng);
+        GaussianGenerator ramGen = new GaussianGenerator(5, 1, rng);
+        GaussianGenerator ioGen = new GaussianGenerator(1000, 20, rng);
+        NumberGenerator<Integer> modifiesDataGen = new ConstantGenerator<>(0);
 
-	Map<String, NumberGenerator<? extends Number>> generators = new HashMap<>();
-	generators.put(StatGenerator.CLOUDLET_LENGTH, cpuGen);
-	generators.put(StatGenerator.CLOUDLET_RAM, ramGen);
-	generators.put(StatGenerator.CLOUDLET_IO, ioGen);
-	generators.put(StatGenerator.CLOUDLET_MODIFIES_DATA, modifiesDataGen);
-	IGenerator<WebCloudlet> asGenerator = new StatGenerator(generators, data);
-	IGenerator<Collection<WebCloudlet>> dbGenerator =
-		new CompositeGenerator<WebCloudlet>(new StatGenerator(generators, data));
+        Map<String, NumberGenerator<? extends Number>> generators = new HashMap<>();
+        generators.put(StatGenerator.CLOUDLET_LENGTH, cpuGen);
+        generators.put(StatGenerator.CLOUDLET_RAM, ramGen);
+        generators.put(StatGenerator.CLOUDLET_IO, ioGen);
+        generators.put(StatGenerator.CLOUDLET_MODIFIES_DATA, modifiesDataGen);
+        IGenerator<WebCloudlet> asGenerator = new StatGenerator(generators, data);
+        IGenerator<Collection<WebCloudlet>> dbGenerator = new CompositeGenerator<WebCloudlet>(new StatGenerator(
+                generators, data));
 
-	List<WebSession> sessions = new ArrayList<>();
-	for (int i = 0; i < sessionNum; i++) {
-	    WebSession session = new WebSession(asGenerator, dbGenerator, broker.getId(), -1, 100);
-	    sessions.add(session);
-	}
-	return sessions;
+        List<WebSession> sessions = new ArrayList<>();
+        for (int i = 0; i < sessionNum; i++) {
+            WebSession session = new WebSession(asGenerator, dbGenerator, broker.getId(), -1, 100);
+            sessions.add(session);
+        }
+        return sessions;
     }
 
     private static Datacenter createDatacenter(final String name) {
 
-	// Here are the steps needed to create a PowerDatacenter:
-	// 1. We need to create a list to store
-	// our machine
-	List<Host> hostList = new ArrayList<Host>();
+        // Here are the steps needed to create a PowerDatacenter:
+        // 1. We need to create a list to store
+        // our machine
+        List<Host> hostList = new ArrayList<Host>();
 
-	// 2. A Machine contains one or more PEs or CPUs/Cores.
-	// In this example, it will have only one core.
-	List<Pe> peList = new ArrayList<>();
-	List<HddPe> hddList = new ArrayList<>();
+        // 2. A Machine contains one or more PEs or CPUs/Cores.
+        // In this example, it will have only one core.
+        List<Pe> peList = new ArrayList<>();
+        List<HddPe> hddList = new ArrayList<>();
 
-	int mips = 1000;
-	int iops = 1000;
+        int mips = 1000;
+        int iops = 1000;
 
-	// 3. Create PEs and add these into a list.
-	peList.add(new Pe(0, new PeProvisionerSimple(mips))); // need to store
-	// Pe id and
-	// MIPS Rating
+        // 3. Create PEs and add these into a list.
+        peList.add(new Pe(0, new PeProvisionerSimple(mips))); // need to store
+        // Pe id and
+        // MIPS Rating
 
-	hddList.add(new HddPe(new PeProvisionerSimple(iops), data));
+        hddList.add(new HddPe(new PeProvisionerSimple(iops), data));
 
-	// 4. Create Host with its id and list of PEs and add them to the list
-	// of machines
-	int ram = 2048; // host memory (MB)
-	long storage = 1000000; // host storage
-	int bw = 10000;
+        // 4. Create Host with its id and list of PEs and add them to the list
+        // of machines
+        int ram = 2048; // host memory (MB)
+        long storage = 1000000; // host storage
+        int bw = 10000;
 
-	// This is our machine
-	hostList.add(new HddHost(new RamProvisionerSimple(ram),
-		new BwProvisionerSimple(bw), storage, peList, hddList,
-		new VmSchedulerTimeShared(peList),
-		new VmDiskScheduler(hddList)));
+        // This is our machine
+        hostList.add(new HddHost(new RamProvisionerSimple(ram), new BwProvisionerSimple(bw), storage, peList, hddList,
+                new VmSchedulerTimeShared(peList), new VmDiskScheduler(hddList)));
 
-	// 5. Create a DatacenterCharacteristics object that stores the
-	// properties of a data center: architecture, OS, list of
-	// Machines, allocation policy: time- or space-shared, time zone
-	// and its price (G$/Pe time unit).
-	String arch = "x86"; // system architecture
-	String os = "Linux"; // operating system
-	String vmm = "Xen";
-	double time_zone = 10.0; // time zone this resource located
-	double cost = 3.0; // the cost of using processing in this resource
-	double costPerMem = 0.05; // the cost of using memory in this resource
-	double costPerStorage = 0.001; // the cost of using storage in this
-	// resource
-	double costPerBw = 0.0; // the cost of using bw in this resource
-	LinkedList<Storage> storageList = new LinkedList<Storage>();
+        // 5. Create a DatacenterCharacteristics object that stores the
+        // properties of a data center: architecture, OS, list of
+        // Machines, allocation policy: time- or space-shared, time zone
+        // and its price (G$/Pe time unit).
+        String arch = "x86"; // system architecture
+        String os = "Linux"; // operating system
+        String vmm = "Xen";
+        double time_zone = 10.0; // time zone this resource located
+        double cost = 3.0; // the cost of using processing in this resource
+        double costPerMem = 0.05; // the cost of using memory in this resource
+        double costPerStorage = 0.001; // the cost of using storage in this
+        // resource
+        double costPerBw = 0.0; // the cost of using bw in this resource
+        LinkedList<Storage> storageList = new LinkedList<Storage>();
 
-	DatacenterCharacteristics characteristics = new DatacenterCharacteristics(
-		arch, os, vmm, hostList, time_zone, cost, costPerMem,
-		costPerStorage, costPerBw);
+        DatacenterCharacteristics characteristics = new DatacenterCharacteristics(arch, os, vmm, hostList, time_zone,
+                cost, costPerMem, costPerStorage, costPerBw);
 
-	// 6. Finally, we need to create a PowerDatacenter object.
-	Datacenter datacenter = null;
-	try {
-	    datacenter = new HddDataCenter(name, characteristics,
-		    new VmAllocationPolicySimple(hostList), storageList, 0);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+        // 6. Finally, we need to create a PowerDatacenter object.
+        Datacenter datacenter = null;
+        try {
+            datacenter = new HddDataCenter(name, characteristics, new VmAllocationPolicySimple(hostList), storageList,
+                    0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	return datacenter;
+        return datacenter;
     }
 
     /**
@@ -248,17 +247,17 @@ public class CloudSimWebExample {
      *            list of Cloudlets
      */
     private static void printCloudletList(final List<Cloudlet> list) {
-	int size = list.size();
-	Cloudlet cloudlet;
+        int size = list.size();
+        Cloudlet cloudlet;
 
-	CustomLog.printLine("\n\n========== OUTPUT ==========");
-	// Printe header line
-	CustomLog.printLine(TextUtil.getCaptionLine(WebCloudlet.class));
+        CustomLog.printLine("\n\n========== OUTPUT ==========");
+        // Printe header line
+        CustomLog.printLine(TextUtil.getCaptionLine(WebCloudlet.class));
 
-	// Print details for each cloudlet
-	for (int i = 0; i < size; i++) {
-	    cloudlet = list.get(i);
-	    CustomLog.print(TextUtil.getTxtLine(cloudlet));
-	}
+        // Print details for each cloudlet
+        for (int i = 0; i < size; i++) {
+            cloudlet = list.get(i);
+            CustomLog.print(TextUtil.getTxtLine(cloudlet));
+        }
     }
 }
