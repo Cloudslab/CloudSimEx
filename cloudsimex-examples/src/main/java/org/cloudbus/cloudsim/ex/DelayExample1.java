0227b8f878e6faa063a77f8566dd76b9ce8cc9bf
Nikolay Grozev <nikolay.grozev@gmail.com>
2014-09-13 01:56:59 +1000

    Fixed compiler warning

--- cloudsimex-examples/src/main/java/org/cloudbus/cloudsim/ex/DelayExample1.java
+++ cloudsimex-examples/src/main/java/org/cloudbus/cloudsim/ex/DelayExample1.java
@@ -12,7 +12,6 @@ import org.cloudbus.cloudsim.CloudletSchedulerTimeShared;
 import org.cloudbus.cloudsim.Datacenter;
 import org.cloudbus.cloudsim.DatacenterCharacteristics;
 import org.cloudbus.cloudsim.Host;
-import org.cloudbus.cloudsim.Log;
 import org.cloudbus.cloudsim.Pe;
 import org.cloudbus.cloudsim.Storage;
 import org.cloudbus.cloudsim.UtilizationModel;
@@ -86,8 +85,6 @@ public class DelayExample1 {
         CloudSim.startSimulation();
         // ...
         CloudSim.stopSimulation();
-        
-        Log.print(cloudlet.getStatus());
     }
 
     /**
