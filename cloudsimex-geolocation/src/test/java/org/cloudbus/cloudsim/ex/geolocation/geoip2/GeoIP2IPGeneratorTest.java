acaebd279b904b769afa80af23dade265b0a5d47
Nikolay Grozev <nikolay.grozev@gmail.com>
2014-10-19 23:51:12 +1100

    Added a new service for test purposes.

--- cloudsimex-geolocation/src/test/java/org/cloudbus/cloudsim/ex/geolocation/geoip2/GeoIP2IPGeneratorTest.java
+++ cloudsimex-geolocation/src/test/java/org/cloudbus/cloudsim/ex/geolocation/geoip2/GeoIP2IPGeneratorTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
